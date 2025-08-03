42f4f6aaf0d4cc7561959dd8c73f504ed5ec4d32
Atta Franta <frantaatta@gmail.com>
2025-08-03 23:26:15 +0000

    Erreur

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // reactStrictMode: true, // Ceci est la valeur par défaut et n'a pas besoin d'être défini explicitement
+};
 
 export default nextConfig;
